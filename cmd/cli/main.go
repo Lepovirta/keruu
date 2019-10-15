e0fd068ce5df163f6f11917809c74af70d07a766
Jaakko Pallari <jaakko.pallari@polarsquad.com>
2019-10-15 20:13:23 +0300

    Concurrent feed fetching and parsing

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"github.com/Lepovirta/keruu/feeds2html"
 	"os"
+	"bufio"
+	"log"
 )
 
 func main() {
+	feedUrls := make([]string, 0, 100)
+	feedScanner := bufio.NewScanner(os.Stdin)
+	for feedScanner.Scan() {
+		feedUrls = append(feedUrls, feedScanner.Text())
+	}
+
+	if err := feedScanner.Err(); err != nil {
+		log.Printf("error reading feed links: %s", err)
+	}
+
 	conf := feeds2html.DefaultConfig()
-	if err := feeds2html.New(conf).FromStream(os.Stdin, os.Stdout); err != nil {
+	conf.FeedURLs = feedUrls
+
+	if err := feeds2html.Run(conf, os.Stdout); err != nil {
 		panic(err)
 	}
 }
