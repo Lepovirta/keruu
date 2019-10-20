0c1af44eff30c18da83b5dfe926263c042738312
Jaakko Pallari <jaakko.pallari@polarsquad.com>
2019-10-20 14:12:08 +0300

    CLI flags
    - Config and output path from CLI flags
    - CLI moved to the root of the repo
    - Config template in usage text

--- feeds2html/config.go
+++ feeds2html/config.go
@@ -1,55 +1,93 @@
 package feeds2html
 
 import (
+	"bufio"
 	"fmt"
 	"html/template"
 	"io"
+	"log"
+	"os"
 	"time"
 
 	"gopkg.in/yaml.v2"
 )
 
+// ConfigTemplateYAML is a YAML template for the config
+const ConfigTemplateYAML = `feeds:
+  - <URL>
+  - <URL>
+  - ...
+fetch:
+  httpTimeout: <DURATION>
+aggregation:
+  title: <STRING>
+  description: <STRING>
+  maxItems: <NUMBER>
+  css: <STRING>
+`
+
 // Config contains the configuration for the entire feed fetching, aggregation, and rendering process
 type Config struct {
-	Feeds       []URL              `yaml:"feeds"`
-	Fetch       *FetchConfig       `yaml:"fetch"`
-	Aggregation *AggregationConfig `yaml:"aggregation"`
+	Feeds       []URL             `yaml:"feeds"`
+	Fetch       FetchConfig       `yaml:"fetch,omitempty"`
+	Aggregation AggregationConfig `yaml:"aggregation,omitempty"`
 }
 
 // FetchConfig contains the feed fetching related configurations
 type FetchConfig struct {
-	HTTPTimeout time.Duration `yaml:"httpTimeout"`
+	HTTPTimeout time.Duration `yaml:"httpTimeout,omitempty"`
 }
 
 // AggregationConfig contains the feed aggregation related configurations
 type AggregationConfig struct {
-	Title       string `yaml:"title"`
-	Description string `yaml:"description"`
-	MaxItems    int    `yaml:"maxItems"`
-	CSSString   string `yaml:"css"`
+	Title       string `yaml:"title,omitempty"`
+	Description string `yaml:"description,omitempty"`
+	MaxItems    int    `yaml:"maxItems,omitempty"`
+	CSSString   string `yaml:"css,omitempty"`
 }
 
-// DefaultConfig generates a configuration with default values
-func DefaultConfig() *Config {
-	return &Config{
-		Fetch: &FetchConfig{
-			HTTPTimeout: time.Second * 10,
-		},
-		Aggregation: &AggregationConfig{
-			Title:       "Keruu",
-			Description: "Aggregation of posts",
-			MaxItems:    1000,
-			CSSString:   defaultCSS,
-		},
+// Init formats the config with default values
+func (c *Config) Init() {
+	c.Feeds = nil
+	c.Fetch = FetchConfig{
+		HTTPTimeout: time.Second * 10,
+	}
+	c.Aggregation = AggregationConfig{
+		Title:       "Keruu",
+		Description: "Aggregation of posts",
+		MaxItems:    1000,
+		CSSString:   defaultCSS,
 	}
 }
 
-// ConfigFromYAML reads the configuration in YAML format
-func ConfigFromYAML(r io.Reader) (c *Config, err error) {
-	c = DefaultConfig()
-	d := yaml.NewDecoder(r)
-	err = d.Decode(c)
-	return
+// FromYAMLFile reads the configuration from a YAML formatted file
+func (c *Config) FromYAMLFile(filename string) (err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("failed to close input file: %s", err)
+		}
+	}()
+	return c.FromYAML(bufio.NewReader(file))
+}
+
+// FromSTDIN reads the configuration from a STDIN in YAML format
+func (c *Config) FromSTDIN() (err error) {
+	return c.FromYAML(os.Stdin)
+}
+
+// FromYAML reads the configuration in YAML format
+func (c *Config) FromYAML(r io.Reader) error {
+	return yaml.NewDecoder(r).Decode(c)
+}
+
+// ToYAML converts the configuration to YAML format
+func (c *Config) ToYAML(w io.Writer) error {
+	return yaml.NewEncoder(w).Encode(c)
 }
 
 // Validate checks if the configuration is valid
---
+++ keruu.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/Lepovirta/keruu/feeds2html"
+)
+
+var configPath string
+var outputPath string
+var config feeds2html.Config
+
+func init() {
+	// Setup CLI flags
+	flag.StringVar(&configPath, "config", "STDIN", "Location of the configuration file")
+	flag.StringVar(&outputPath, "output", "STDOUT", "Location of the HTML output file")
+
+	// Init config
+	config.Init()
+
+	// Setup custom usage function
+	defaultUsage := flag.Usage
+	flag.Usage = func() {
+		defaultUsage()
+		fmt.Fprintf(flag.CommandLine.Output(), "\nConfiguration format:\n\n%s", feeds2html.ConfigTemplateYAML)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if err := readConfig(); err != nil {
+		log.Fatalf("failed to read config from STDIN: %s", err)
+	}
+
+	if err := writeOutput(func (w io.Writer) error {
+		return feeds2html.Run(&config, w) 
+	}); err != nil {
+		log.Panicf("feed aggregation failed: %s", err)
+	}
+}
+
+func readConfig() error {
+	if isSTDIN() {
+		return config.FromSTDIN()
+	}
+	return config.FromYAMLFile(configPath)
+}
+
+func writeOutput(f func(io.Writer) error) error {
+	var writer io.Writer
+	if isSTDOUT() {
+		writer = os.Stdout
+	} else {
+		file, err := os.Create(outputPath)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			err := file.Close()
+			if err != nil {
+				log.Printf("failed to close output file: %s", err)
+			}
+		}()
+		writer = file
+	}
+
+	bufWriter := bufio.NewWriter(writer)
+	if err := f(bufWriter); err != nil {
+		return err
+	}
+	return bufWriter.Flush()
+}
+
+func isSTDIN() bool {
+	switch configPath {
+	case "", "-", "STDIN":
+		return true
+	}
+	return false
+}
+
+func isSTDOUT() bool {
+	switch outputPath {
+	case "", "-", "STDOUT":
+		return true
+	}
+	return false
+}
