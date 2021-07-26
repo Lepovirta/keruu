17cb18c6fd9e26828c97663dfb05cf7e53bbccaa
Jaakko Pallari <jkpl@lepovirta.org>
2021-07-26 21:13:10 +0300

    Post grouping, new coat of paint, embedded assets
    - Posts are grouped by month. This adds a handy separator between posts.
    - The style is updated to be a bit more minimalistic in color.
    - Assets are pulled from files during compilation instead of strings.

--- internal/aggregation/aggregation.go
+++ internal/aggregation/aggregation.go
@@ -8,19 +8,59 @@ import (
 	"gitlab.com/lepovirta/keruu/internal/feed"
 )
 
+type PostGroup struct {
+	Name  string
+	Posts []*feed.Post
+}
+
 type Aggregation struct {
-	Config *Config
-	Time   time.Time
-	Posts  []*feed.Post
+	Config     *Config
+	Time       time.Time
+	PostGroups []PostGroup
 }
 
 func New(config *Config, posts []*feed.Post) *Aggregation {
 	sortPostsByTime(posts)
+	posts = posts[0:config.MaxPosts]
+
 	return &Aggregation{
-		Config: config,
-		Time: time.Now(),
-		Posts: posts[0:config.MaxPosts],
+		Config:     config,
+		Time:       time.Now(),
+		PostGroups: monthlyGroups(posts),
 	}
+}
+
+func groupPosts(posts []*feed.Post, groupName func(*feed.Post) string) []PostGroup {
+	groupIndex := 0
+	groups := make([]PostGroup, 5*12)
+	for _, post := range posts {
+		name := groupName(post)
+		group := &groups[groupIndex]
+
+		if group.Name != "" && group.Name != name {
+			// If the current group doesn't match with the name,
+			// create a new group for the post.
+			groupIndex += 1
+			group = &groups[groupIndex]
+		}
+
+		if group.Name == "" {
+			// Empty group => initialize
+			group.Name = name
+			group.Posts = make([]*feed.Post, 100)
+			group.Posts = append(group.Posts, post)
+		} else {
+			// Existing group => append
+			group.Posts = append(group.Posts, post)
+		}
+	}
+	return groups
+}
+
+func monthlyGroups(posts []*feed.Post) []PostGroup {
+	return groupPosts(posts, func(p *feed.Post) string {
+		return p.Time.Format("01/2006")
+	})
 }
 
 func sortPostsByTime(posts []*feed.Post) {
--- internal/aggregation/template.go
+++ internal/aggregation/template.go
@@ -1,227 +1,25 @@
 package aggregation
 
 import (
+	_ "embed"
 	"html/template"
 	"io"
 )
 
-const (
-	templateStr = `<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width,initial-scale=1" name="viewport">
-  <title>{{ .Config.Title }}</title>
-  <meta name="description" content="{{ .Config.Description }}">
-  <meta property="og:title" content="{{ .Config.Title }}">
-  <meta property="og:site_name" content="{{ .Config.Title }}">
-  <meta property="og:type" content="website">
-  <meta property="og:description" content="{{ .Config.Description }}">
-  <link rel="icon" href="data:,">
-  <style>{{ .Config.CSS }}</style>
-</head>
-<body>
-<main>
-  <header>
-    <h1 class="title">{{ .Config.Title }}</h1>
-    <p class="description">{{ .Config.Description }}</p>
-  </header>
-  <ul class="post-list">
-  {{- range $_, $post := .Posts }}
-  {{- with $post }}
-    <li>
-      <div class="post-head">
-        <a class="post-title" href="{{ .Link }}">{{ .Title }}</a>
-      </div>
-      <div class="post-meta">
-        <a class="post-feed" href="{{ .FeedLink }}">{{ .FeedName }}</a>
-        <time class="post-time" datetime="{{ .FormattedTime }}">{{ .FormattedTime }}</time>
-        {{- if .ExtLinks }}
-        <ul class="post-ext-links">
-        {{- range $_, $extLink := .ExtLinks }}
-          <li><a href="{{ $extLink.Link }}">{{ $extLink.Name }}</a></li>
-        {{- end }}
-        </ul>
-        {{- end }}
-      </div>
-    </li>
-  {{- end }}
-  {{- end }}
-  </ul>
-  <footer>
-    Generated using <a href="https://gitlab.com/lepovirta/keruu">keruu</a>
-    at {{ .FormattedTime }}.
-  </footer>
-</main>
-</body>
-</html>
-`
-
-	defaultCSS = `body {
-  background-color: #f7f0ff;
-  padding: 0;
-  margin: 0;
-  font-family: sans-serif;
-}
-
-main {
-  max-width: 760px;
-  margin: 20px auto;
-}
-
-header {
-  background-color: #44118d;
-  color: #eee;
-  padding: 5px 20px;
-  margin: 0;
-}
-
-a {
-  color: #00e;
-}
-
-a:visited {
-  color: #551abb;
-}
-
-footer {
-  padding: 10px;
-  font-size: 0.9em;
-  text-align: right;
-  color: #444;
-}
-
-.title {
-  margin: 2px 0;
-}
-
-.description {
-  margin: 2px 0;
-}
-
-.post-list {
-  background-color: white;
-  margin: 0;
-  padding: 20px;
-}
-
-.post-list a {
-  text-decoration: none;
-}
-
-.post-list a:hover {
-  text-decoration: underline;
-}
-
-.post-list li {
-  list-style: none;
-  padding: 5px 0;
-}
-
-.post-head {
-  font-size: 1.2em;
-}
-
-.post-meta {
-  font-size: 0.9em;
-}
-
-.post-count::after {
-  content: '.';
-}
-
-.post-feed {
-  color: #f44;
-  text-decoration: none;
-}
-
-.post-feed-name:visited {
-  color: #b22;
-}
-
-.post-time::before {
-  content: '| ';
-}
-
-.post-time {
-  font-size: 0.9em;
-  color: #484;
-}
-
-.post-ext-links {
-  display: inline;
-  padding: 0;
-}
-
-.post-ext-links::before {
-  content: '|';
-  color: #18798e;
-}
-
-.post-ext-links li {
-  display: inline;
-}
-
-.post-ext-links li::after {
-  content: ',';
-}
-
-.post-ext-links li:nth-last-child(1)::after {
-  content: '';
-}
-
-.post-ext-links a {
-  color: #18798e;
-}
-
-.post-ext-links a:visited {
-  color: #104754;
-}
-
-@media screen and (max-width:760px) {
-  main {
-    max-width: auto;
-    margin: 0;
-  }
-}
-
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #113;
-  }
-  a {
-    color: #aef;
-  }
-  a:visited {
-    color: #8be;
-  }
-  footer {
-    color: #baf;
-  }
-  .post-list {
-    background-color: #214;
-  }
-  .post-feed-name {
-    color: #f88;
-  }
-  .post-feed-name:visited {
-    color: #b66;
-  }
-  .post-time {
-    color: #8f8;
-  }
-}
-`
-)
-
 var (
 	htmlTemplate *template.Template
+
+	//go:embed style.css
+	defaultCSS string
+
+	//go:embed template.html
+	htmlTemplateStr string
 )
 
 func init() {
 	var err error
 
-	htmlTemplate, err = template.New("").Parse(templateStr)
+	htmlTemplate, err = template.New("").Parse(htmlTemplateStr)
 	if err != nil {
 		panic(err)
 	}
